2b4afb31797b74e062e400c7948b00d9487d76d2
berrekucuk <berrekucuk@hotmail.com>
2024-09-16 22:07:52 +0300

    Add project files.

---
+++ Activity.DAL/ORM/ActivityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity.DAL.ORM
+{
+    public class ActivityDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-2K18C6FK\\SQLEXPRESS05;Database=ActivityDb;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
